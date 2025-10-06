815a18498d9a9d250ac92250ad4160dbc0bcc4ae
CaioRibeiro <caiopr0102@gmail.com>
2025-10-06 18:21:16 -0300

    Adicionando alterações propostas

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia("121314", "modelo 1", "1111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone("111222", "Iphone 12", "2222222", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
 // TODO: Realizar os testes com as classes Nokia e Iphone
